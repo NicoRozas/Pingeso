cce9272c64d71c0387ab88a277ed7889b0ebb970
Aracelly Altamirano <arapuntocom@gmail.com>
2015-12-07 17:41:16 -0300

    nuevo proyecto
    contiene las dependencias de pf5.3.- no tiene vistas ni mb. EJB en
    construcción.

---
+++ sml3/sml3-ejb/src/main/java/facade/TipoUsuarioFacade.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package facade;
+
+import entity.TipoUsuario;
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+/**
+ *
+ * @author Aracelly
+ */
+@Stateless
+public class TipoUsuarioFacade extends AbstractFacade<TipoUsuario> implements TipoUsuarioFacadeLocal {
+    @PersistenceContext(unitName = "com.raracelly_sml3-ejb_ejb_3.0-SNAPSHOTPU")
+    private EntityManager em;
+
+    @Override
+    protected EntityManager getEntityManager() {
+        return em;
+    }
+
+    public TipoUsuarioFacade() {
+        super(TipoUsuario.class);
+    }
+    
+}
