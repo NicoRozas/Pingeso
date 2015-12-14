ecbc74834700b3f75340d9cb2cd8e993a727e94a
riquelmj <riquemj@hotmail.com>
2015-12-14 14:37:58 -0300

    'qweasd'

--- sml1/sml1-ejb/target/generated-sources/annotations/entity/EdicionFormulario_.java
+++ sml1/sml1-ejb/target/generated-sources/annotations/entity/EdicionFormulario_.java
@@ -7,7 +7,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2015-12-07T04:05:08")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2015-12-14T14:34:17")
 @StaticMetamodel(EdicionFormulario.class)
 public class EdicionFormulario_ { 
 
--- sml1/sml1-ejb/target/generated-sources/annotations/entity/FormularioEvidenciaPK_.java
+++ sml1/sml1-ejb/target/generated-sources/annotations/entity/FormularioEvidenciaPK_.java
@@ -4,7 +4,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2015-12-07T04:05:08")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2015-12-14T14:34:17")
 @StaticMetamodel(FormularioEvidenciaPK.class)
 public class FormularioEvidenciaPK_ { 
 
